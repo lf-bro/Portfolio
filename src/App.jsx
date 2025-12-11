82a6bc60577520472e5508f132fb25f05e9296ea
L.F. Pemi Magar <lfpemimagar@gmail.com>
2025-12-11 15:24:50 +0545

    web  build

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex items-center justify-center h-screen w-screen">
+      Under Construction
+    </div>
+  );
+};
+
+export default App;
